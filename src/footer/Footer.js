b4c4edd04c97fe6395c0f8171733b3330dc17cc7
AndreiAkimochkin <AndreiSverige@gmail.com>
2021-01-01 20:01:31 +0100

    V 2. ADD socila

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,25 +1,38 @@
 import style from './footer.module.scss';
 import React from 'react'
 import styleContainer from '../common/styles/container.module.scss'
-import {Net} from "./socialNets/Net";
 
 export const Footer = () => {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Akimochkin Andrei</h2>
-                <div className={style.footer}>
-                    <Net title={'Facebook'}/>
-                    <Net title={'Twitter'}/>
-                    <Net title={'LinkedIn'}/>
-                    <Net title={'GitHub'}/>
+                <h3 className={style.title}>don't be shy!</h3>
+                <p className={style.text}>Feel free to get in touch with me.
+                    I am always open to discussing new projects,
+                    creative ideas or opportunities to be part of your visions.</p>
+                <div className={style.social}>
+                    <div className={style.github}>
+                        <a href="#" target="_blank"><i className="fa fa-github fa-2x"></i></a>
+                    </div>
+                    <div className={style.linkedin}>
+                        <a href="#" target="_blank"><i className="fa fa-linkedin fa-2x"></i></a>
+                    </div>
+                    <div className={style.google}>
+                        <a href="#" target="_blank"><i className="fa fa-google-plus fa-2x"></i></a>
+                    </div>
+                    <div className={style.instagram}>
+                        <a href="#" target="_blank"><i className="fa fa-instagram fa-2x"></i></a>
+                    </div>
+                    <div className={style.whatsupp}>
+                        <a href="#" target="_blank"><i className="fa fa-whatsapp fa-2x"></i></a>
+                    </div>
                 </div>
-                <div className={style.notation}>
-                    <h3>
-                        {'2020, All rights reserved'}
-                    </h3>
+                {/*<div className={style.notation}>*/}
+                {/*    <h3>*/}
+                {/*        {'2020, All rights reserved'}*/}
+                {/*    </h3>*/}
 
-                </div>
+                {/*</div>*/}
             </div>
 
         </div>
