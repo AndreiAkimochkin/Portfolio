c93d4e938c92635d905bf107cb70bf9a46d1d9c8
AndreiAkimochkin <AndreiSverige@gmail.com>
2021-01-01 20:29:34 +0100

    V 2. ADD socila

--- src/main/Main.js
+++ src/main/Main.js
@@ -8,7 +8,7 @@ export const Main = () => {
             <div className={styleContainer.container}>
                 <div className={style.text}>
                     <h2 className={style.h2}>Hi THERE!</h2>
-                    <h1 className={style.h1}> <span className={style.span}>I'M </span>
+                    <h1 className={style.h1}> <span className={style.span}>I'M  </span>
                         ANDREI AKIMOCHKIN</h1>
                     <p className={style.p}>I'm a front‑end developer focused on crafting clean & user‑friendly experiences,
                         I am passionate about building excellent software that improves the lives of those around me.</p>
